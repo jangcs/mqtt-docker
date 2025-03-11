031110286a1fb44283a36468b006d55cb63872f7
jangcs <jangcs@gmail.com>
2025-03-11 18:03:12 +0900

    Update r.sh
    nvidia-docker run

--- Publisher/r.sh
+++ Publisher/r.sh
@@ -1,3 +1,5 @@
 #sudo docker run -it --name publisher --network="host" mqtt-publish:v1
 docker run -it --rm --name publisher --network="host" mqtt-publish:v1
 
+#nvidia-docker run -it --rm --gpus all --name publisher --network="host" -e PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python mqtt-publish:v1
+
